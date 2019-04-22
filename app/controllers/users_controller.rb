721ffa885b9d511b8dc77a10a064f649ecd5b674
James Okunlade <james.okunlade@gmail.com>
2019-04-22 23:08:51 +0100

    Signup implemented

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,30 @@
 class UsersController < ApplicationController
+
+
+
+    def new 
+        @user = User.new 
+    end 
+
+    def create 
+        @user = User.new(user_params)
+        if @user.save
+            flash[:success] = "User created successfully."
+            redirect_to root_url
+          else
+            render 'new'
+        end
+    end
+
+    def show
+        @user = User.find(params[:id])
+    end
+
+
+    private
+
+        def user_params
+            params.require(:user).permit(:name, :email, :password, :password_confirmation)
+        end
+
 end
